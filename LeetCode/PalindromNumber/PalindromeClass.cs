bf45af62748eaaaad0cb5f6a3a019e05544be250
VeselinValkanov <73895790+kaiovasa1999-ux@users.noreply.github.com>
2022-01-17 12:01:00 +0200

    Palindrome Numbers checker

---
+++ LeetCode/PalindromNumber/PalindromeClass.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+
+namespace LeetCode
+{
+    public class PalindromeClass
+    {
+        public string IsPalindrome(int x)
+        {
+            var num = x.ToString();
+            var x1 = "";
+            foreach (var item in num)
+            {
+                x1 += item;
+            }
+
+            var x1List = x1.ToList();
+            var x2 = "";
+
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                x2 += x1[i];
+            }
+            if (x2 != num)
+            {
+                return "False";
+            }
+            return "Palindromes";
+        }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new PalindromeClass();
+            Console.Write(x.IsPalindrome(535));
+        }
+    }
+}
