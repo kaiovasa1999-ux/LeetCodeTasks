e20c19e389cea1d8115124473d7379311d5e82fb
VeselinValkanov <73895790+kaiovasa1999-ux@users.noreply.github.com>
2022-01-17 13:18:34 +0200

    Double Array

---
+++ LeetCode/ArrayConcatenation/ArrayConcatenationClass.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace LeetCode.ArrayConcatenation
+{
+
+   public class ArrayConcatenationClass
+    {
+        public int[] Arr(int[] nums)
+        {
+            int[] array2 = new int[nums.Length * 2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                array2[i] = nums[i];
+            }
+            var index = nums.Length;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                array2[index] = nums[i];
+                index++;
+            }
+            return array2;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,13 +1,17 @@
 ﻿namespace LeetCode
 {
     using System;
+    using LeetCode.ArrayConcatenation;
 
     class Program
     {
         static void Main(string[] args)
         {
-            var x = new PalindromeClass();
-            Console.Write(x.IsPalindrome(535));
+            var x = new ArrayConcatenationClass();
+
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.Write(x.Arr(array));
         }
     }
 }
