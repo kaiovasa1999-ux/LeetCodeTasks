42e9e1cb76a721dbe2e33361b5ec7040828f9ebe
VeselinValkanov <73895790+kaiovasa1999-ux@users.noreply.github.com>
2022-01-17 15:32:57 +0200

    WordPattern/PatternChecker.cs

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -2,16 +2,14 @@
 {
     using System;
     using LeetCode.ArrayConcatenation;
+    using LeetCode.WordPattern;
 
     class Program
     {
         static void Main(string[] args)
         {
-            var x = new ArrayConcatenationClass();
-
-            int[] array = new int[] { 1, 2, 3, 4, 5 };
-
-            Console.Write(x.Arr(array));
+            var x = new PatternChecker();
+            Console.Write(x.Checker("aaa", "aba asba aba"));
         }
     }
 }
---
+++ LeetCode/WordPattern/PatternChecker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.WordPattern
+{
+    public class PatternChecker
+    {
+        public string Checker(string pattern, string words)
+        {
+            var wordsList = words.Split().ToList();
+            var patternList = new List<Char>();
+            foreach (var item in pattern)
+            {
+                patternList.Add(item);
+            }
+
+            if(pattern.Length != wordsList.Count())
+            {
+                return "False";
+            }
+            var x = 0;
+            for (int i = 0; i < patternList.Count(); i++)
+            {
+                if (i == 0)
+                {
+                   if(patternList[0] == patternList[1] && wordsList[0] == wordsList[1])
+                    {
+                        x++;
+                    }
+                }
+
+                if(i > 0)
+                {
+                    if (patternList[i] == patternList[i - 1] && wordsList[i] == wordsList[i - 1])
+                    {
+                        x++;
+                    }
+                }
+           
+            }
+            if(x < patternList.Count())
+            {
+                return "False";
+            }
+            return "Gooood!!!";
+
+        }
+    }
+}
