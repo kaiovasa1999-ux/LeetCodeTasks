b6e121032ac92cf210555902fc8eb73d61224021
VeselinValkanov <73895790+kaiovasa1999-ux@users.noreply.github.com>
2022-02-03 11:50:23 +0200

    Merge Two list and sort

--- LeetCode/LongetVAlidParentheses/ParenthesesReader.cs
+++ LeetCode/LongetVAlidParentheses/ParenthesesReader.cs
@@ -8,50 +8,31 @@
 
     public class ParenthesesReader
     {
-        public int findMaxLen(string str)
+        public  int findMaxLen(string str)
         {
             int n = str.Length;
 
-            // Create a stack and push -1 as
-            // initial index to it.
             Stack<int> stk = new Stack<int>();
             stk.Push(-1);
-            // Initialize result
             int result = 0;
 
-            // Traverse all characters of
-            // given string
             for (int i = 0; i < n; i++)
             {
-                // If opening bracket, push
-                // index of it
                 if (str[i] == '(')
                 {
                     stk.Push(i);
                 }
 
-                else // If closing bracket,
-                     // i.e.,str[i] = ')'
+                else 
                 {
-                    // Pop the previous opening
-                    // bracket's index
                     if (stk.Count > 0)
                         stk.Pop();
 
-                    // Check if this length formed
-                    // with base of current valid
-                    // substring is more than max
-                    // so far
                     if (stk.Count > 0)
                     {
-                        result
-                            = Math.Max(result,
-                                       i - stk.Peek());
+                        result = Math.Max(result, i - stk.Peek());
                     }
 
-                    // If stack is empty. push current
-                    // index as base for next valid
-                    // substring (if any)
                     else
                     {
                         stk.Push(i);
@@ -62,18 +43,6 @@
             return result;
         }
 
-        //// Driver Code
-        //public static void Main(string[] args)
-        //{
-        //    string str = "((()()";
-
-        //    // Function call
-        //    Console.WriteLine(findMaxLen(str));
-
-        //    str = "()(()))))";
-
-        //    // Function call
-        //    Console.WriteLine(findMaxLen(str));
-        //}
+      
     }
 }
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,26 +1,37 @@
 ﻿namespace LeetCode
 {
     using System;
-    using LeetCode.ArrayConcatenation;
-    using LeetCode.WordPattern;
-    using LeetCode.Password;
-    using LeetCode.LongestPalindrome;
-    using LeetCode.TargetNumInArr;
-    using LeetCode.ContainerWithMostWater;
-    using LeetCode.ReverseIntiger;
-    using LeetCode.DetectCapital;
-    using LeetCode.FromStirngToInt;
-    using LeetCode.PointsInTheArea;
-    using LeetCode.LongetVAlidParentheses;
+    using System.Collections.Generic;
+    using LeetCode.PrentesesGeneration;
+
     class Program
     {
         static void Main(string[] args)
         {
-            var x = new ParenthesesReader();
+            var x = new ListMerger();
+            var l1 = new List<int>();
+            var l2 = new List<int>();
+          
+            l1.Add(1);
+            l1.Add(2);
+            l1.Add(3);
+            l1.Add(4);
+            l1.Add(5);
+            l1.Add(8);
+            l2.Add(3);
+            l2.Add(31);
+            l2.Add(6);
+            l2.Add(2);
+            l2.Add(7);
 
-            var text = ")()())";
+            var res = "";
+            foreach (var item in x.MergeTwoLists(l1, l2))
+            {
+                res += item + ", ";
+            }
 
-            Console.WriteLine(x.findMaxLen(text));
+            Console.WriteLine(res);
+
         }
     }
 }
